f5eac47e7f281a5839c20d24c0d8b4e0d879932c
zmy <18340323052@139.com>
2019-08-24 23:13:30 +0800

    欢迎页面和404页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,23 @@ import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
 import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'home', component: Home }
+    {
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎页面  如果子路由有名字  父级路由需要删除
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    },
+    { path: '*', name: '404', component: NotFound }
   ]
 })
 
