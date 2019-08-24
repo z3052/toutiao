da3a7788a5115a5d2137bc830ec31eb5fc98f9fe
zmy <18340323052@139.com>
2019-08-24 22:55:07 +0800

    welcome

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,20 @@ import VueRouter from 'vue-router'
 
 import Login from '@/views/login'
 import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'home', component: Home }
+    {
+      path: '/',
+      component: Home,
+      children: [
+        { path: '/', name: 'welcome', component: Welcome }
+      ]
+    }
   ]
 })
 
